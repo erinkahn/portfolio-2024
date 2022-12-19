28e145c370ed9b7904d460e7d121442a71248cf6
erin <erinnicolekahn@gmail.com>
2022-12-19 17:29:35 -0600

    responsive fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,70 +17,72 @@ export default function Footer() {
 			role="contentinfo" 
 			className="section"
 		>
-			<span id="footer">
-				<button 
-					className="btn-scroll" 
-					onClick={scrollToTop}
-				>
-					<span className="sr-only">scroll to the top of the page</span>
-				</button>
+			<div className="outer-wrapper">
+				<span id="footer">
+					<button 
+						className="btn-scroll" 
+						onClick={scrollToTop}
+					>
+						<span className="sr-only">scroll to the top of the page</span>
+					</button>
 
-				<div className="inner-footer-wrapper">
-					<div className="row one">
-						{
-							location.pathname === '/' ? (
-								<h6>Let's Brainstorm</h6>
-							) : (
-								<h3>Let's Brainstorm</h3>
-							)
-						}
-						<ul className="footer-nav-list">
-							{navData.navigation.map((item) => (
-								<li
-									key={`nav-${item.id}`}
-									className="nav-item"
-								>
-									<NavLink
-										className="nav-link"
-										to={item.linkTo}
+					<div className="inner-footer-wrapper">
+						<div className="row one">
+							{
+								location.pathname === '/' ? (
+									<h6>Let's Brainstorm</h6>
+								) : (
+									<h3>Let's Brainstorm</h3>
+								)
+							}
+							<ul className="footer-nav-list">
+								{navData.navigation.map((item) => (
+									<li
+										key={`nav-${item.id}`}
+										className="nav-item"
 									>
-										{item.pageTitle}
-										<span className="sr-only">Page</span>
-									</NavLink>
-								</li>
-							))}
-						</ul>
-					</div>
+										<NavLink
+											className="nav-link"
+											to={item.linkTo}
+										>
+											{item.pageTitle}
+											<span className="sr-only">Page</span>
+										</NavLink>
+									</li>
+								))}
+							</ul>
+						</div>
 
-					<div className="btn-wrapper">
-						<a href="mailto:erinnicolekahn@gmail.com">Get In Touch</a>
-					</div>
+						<div className="btn-wrapper">
+							<a href="mailto:erinnicolekahn@gmail.com">Get In Touch</a>
+						</div>
 
-					<div className="row two">
-						<p className="copyright">Copyright &copy; {today}. All rights reserved.</p>					
-						<ul className="social">
-							{navData.socialIcons.map((icon) => (
-								<li
-									className="icon-list-item"
-									key={`icon-${icon.id}`}
-								>
-									<a
-										className={`icon ${icon.siteName}`}
-										target="_blank"
-										rel="noreferrer"
-										href={icon.url}
+						<div className="row two">
+							<p className="copyright">Copyright &copy; {today}. All rights reserved.</p>					
+							<ul className="social">
+								{navData.socialIcons.map((icon) => (
+									<li
+										className="icon-list-item"
+										key={`icon-${icon.id}`}
 									>
-										<span className="sr-only">
-											{icon.siteName}
-										</span>
-									</a>
-								</li>
-							))}
-						</ul>
+										<a
+											className={`icon ${icon.siteName}`}
+											target="_blank"
+											rel="noreferrer"
+											href={icon.url}
+										>
+											<span className="sr-only">
+												{icon.siteName}
+											</span>
+										</a>
+									</li>
+								))}
+							</ul>
+						</div>
 					</div>
-				</div>
-				{/* <Instafeed /> */}
-			</span>
+					{/* <Instafeed /> */}
+				</span>
+			</div>
 		</footer>
 	);
 }
